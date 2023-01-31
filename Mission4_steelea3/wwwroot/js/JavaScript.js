d5e5175758f7e7ea5832a2266758c0f9b6034f0a
andreasteel <102710199+andreasteel@users.noreply.github.com>
2023-01-31 10:30:26 -0700

    edit

--- Mission4_steelea3/wwwroot/js/JavaScript.js
+++ Mission4_steelea3/wwwroot/js/JavaScript.js
@@ -1,47 +1,57 @@
-﻿$("#results").click(function () {
-    //alert("from: " + $("#txtfrom").val() +
-    //    "subject:" + $("#txtsubject").val());
-    iLetterGrade = '';
-    iGrade = $("#assignments").val() + $("#groupproject").val() + $("#quizzes").val() + $("#midterm").val() + $("#final").val() + $("#intex").val();
+﻿//function to get the results
+$("#results").click(function () {
+   // getting grade percent
+    iGrade = ($("#assignments").val() * .5) +
+        ($("#groupprojects").val() * .1) +
+        ($("#quizzes").val() * .1) +
+        ($("#midterm").val() * .1) +
+        ($("#final").val() * .1) +
+        ($("#intex").val() * .1)
+    iGrade = iGrade.toFixed(2);
+
+    //getting grade
     if (iGrade >= 94) {
-        iLetterGrade = 'A'
+        iLetterGrade = 'A';
     }
     else if (iGrade >= 90) {
-        iLetterGrade = 'A-'
+        iLetterGrade = 'A-';
     }
     else if (iGrade >= 87) {
-        iLetterGrade = 'B+'
+        iLetterGrade = 'B+';
     }
     else if (iGrade >= 84) {
-        iLetterGrade = 'B'
+        iLetterGrade = 'B';
     }
     else if (iGrade >= 80) {
         iLetterGrade = 'B-'
     }
     else if (iGrade >= 77) {
-        iLetterGrade = 'C+'
+        iLetterGrade = 'C+';
     }
     else if (iGrade >= 74) {
-        iLetterGrade = 'C'
+        iLetterGrade = 'C';
     }
     else if (iGrade >= 70) {
-        iLetterGrade = 'C-'
+        iLetterGrade = 'C-';
     }
     else if (iGrade >= 67) {
-        iLetterGrade = 'D+'
+        iLetterGrade = 'D+';
     }
     else if (iGrade >= 64) {
-        iLetterGrade = 'D'
+        iLetterGrade = 'D';
     }
     else if (iGrade >= 60) {
-        iLetterGrade = 'D-'
+        iLetterGrade = 'D-';
     }
     else if (iGrade >= 60) {
-        iLetterGrade = 'E'
+        iLetterGrade = 'E';
     }
     else {
         iLetterGrade = 'None'
     }
 
-    console.log(iLetterGrade)
+    $('#finalGrade').show()
+    $("#finalGrade").html('<b>Grade: </b>' + iGrade + '%' + '<br>' + '<b>Letter Grade: </b>' + iLetterGrade)
+    $("#reload").show()
+
 })
