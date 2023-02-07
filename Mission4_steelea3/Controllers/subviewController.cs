17673804bcc4fdfcac97f8999fadd43139c23e97
andreasteel <102710199+andreasteel@users.noreply.github.com>
2023-02-06 21:38:55 -0700

    mission 5 start

---
+++ Mission4_steelea3/Controllers/subviewController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_steelea3.Controllers
+{
+    public class subviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
